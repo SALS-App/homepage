47f872dfc0b00d5d3177a3ab5cea1d241d811567
Timm Ortloff <timmortloff@Timms-Air.fritz.box>
2020-05-21 18:55:16 +0200

    Removed ununsed imports

--- src/app/providers/blob-downloader.service.ts
+++ src/app/providers/blob-downloader.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subscription, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
