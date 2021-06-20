306daa42043e210be45943d7caed62e4675bec3d
Timm Ortloff <me@tortloff.de>
2021-06-20 15:32:48 +0200

    Cleaned up home component and added download method

--- src/app/sites/home/home.component.ts
+++ src/app/sites/home/home.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { SelectModalDialog } from './select-modal/select-modal';
-import { SharedService } from 'src/app/providers/shared.service';
-import { HttpClient, HttpEventType, HttpRequest, HttpResponse, HttpHeaders } from '@angular/common/http';
-import { BlobDownloaderService } from 'src/app/providers/blob-downloader.service';
-import { FileSaverService } from 'ngx-filesaver';
+import { DownloadComponent } from 'src/app/components/download/download.component';
 
 @Component({
   selector: 'app-home',
@@ -13,57 +9,11 @@ import { FileSaverService } from 'ngx-filesaver';
 })
 export class HomeComponent {
 
-
-  isDownloading: boolean = false;
-
-  private ProvierDialog: any;
-
-  dlPercent: string;
-
   constructor(
-    private dialog: MatDialog,
-    private ss: SharedService,
-    private httpClient: HttpClient,
-    private bloDown: BlobDownloaderService,
-    private fSaver: FileSaverService
-    ) {
-    ss.getEmittedValue().subscribe((res: any) => {
-      this.downloadFile(res);
-    })
+    private dialog: MatDialog
+  ) { }
+
+  download(): void {
+    this.dialog.open(DownloadComponent, { disableClose: true });
   }
-
-  openDialog(): void {
-    this.ProvierDialog =  this.dialog.open(SelectModalDialog, {
-      width: '50%'
-    });
-
-  }
-
-
-  async downloadFile(res: string) {
-    console.log(res);
-    const req = new HttpRequest('GET', await this.bloDown.getDLLink(res), {
-      reportProgress: true,
-      responseType: 'blob',
-      headers: new HttpHeaders({ 'Content-Type': 'application/octet-stream' }) 
-    });
-    
-    this.httpClient.request(req).subscribe(event => {
-      if (event.type === HttpEventType.DownloadProgress) {
-        this.ProvierDialog.close();
-        this.isDownloading = true;
-
-        
-        this.dlPercent = Math.round(100 * event.loaded / event.total).toString();
-        this.ss.changeDownloadVal({
-          'loaded': event.loaded,
-          'total': event.total
-        });
-      } else if (event instanceof HttpResponse) {
-        this.fSaver.save((<any>event.body), `${res} Installer.exe`);
-        this.isDownloading = false;
-      }
-    });
-  }
-
 }
