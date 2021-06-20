8010f661e87fe7a82627cbae1563545ad3ebf6ac
Timm Ortloff <me@tortloff.de>
2021-06-20 15:32:28 +0200

    Removed Shared Service and Select Modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,6 @@ import { FileSaverModule } from 'ngx-filesaver';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './sites/home/home.component';
-import { SelectModalDialog } from './sites/home/select-modal/select-modal';
-import { SharedService } from './providers/shared.service';
 import { BlobDownloaderService } from './providers/blob-downloader.service';
 import { DownloadComponent } from './components/download/download.component';
 import { MainComponent } from './components/home/main/main.component';
@@ -27,7 +25,6 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
-    SelectModalDialog,
     DownloadComponent,
     MainComponent
   ],
@@ -40,10 +37,9 @@ const appRoutes: Routes = [
     SharedModule
   ],
   providers: [
-    SharedService,
     BlobDownloaderService
   ],
   bootstrap: [AppComponent],
-  entryComponents: [SelectModalDialog]
+  entryComponents: []
 })
 export class AppModule { }
