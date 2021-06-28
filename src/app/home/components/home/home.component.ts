28c72c0179d28ac707d7e9bc296c9b3de2fb5338
Timm Ortloff <me@tortloff.de>
2021-06-28 16:15:28 +0200

    Enabled outbount tracking for home component

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DownloadComponent } from 'src/app/download/components/download/download.component';
+import { TelemetryService } from 'src/app/telemetry/services/telemetry.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   constructor(
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private telemetry: TelemetryService
   ) { }
+
+  ngOnInit() {
+    this.telemetry.client.enableAutoOutboundTracking();
+  }
 
   download(): void {
     this.dialog.open(DownloadComponent, { disableClose: true });
