0116ee281a9e26dc65da38b3f66301950c2a4f6e
Timm Ortloff <me@tortloff.de>
2022-05-09 04:45:44 +0200

    Added Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <h1>SALS</h1>
+  <v-app>
+    <v-main>
+      <h1>Hey</h1>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+
+  }
+})
+</script>
