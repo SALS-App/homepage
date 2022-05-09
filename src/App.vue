227e3ebcaafa5b73e71d4e6815b534ca07d3619c
Timm Ortloff <me@tortloff.de>
2022-05-09 06:10:58 +0200

    Added components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
   <v-app>
-    <v-main>
-      <h1>Hey</h1>
-    </v-main>
+    <div class="bg">
+      <v-main class="container">
+        <Navbar />
+
+        <Landing />
+
+        <section id="section2">
+          <div></div>
+        </section>
+      </v-main>
+    </div>
   </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import logo from './assets/images/logo.png';
+import Navbar from './components/Navbar.vue';
+import Landing from './components/home/Landing.vue';
 
 export default defineComponent({
+  components: { Navbar, Landing },
   setup() {
-
+    return { logo };
   }
 })
 </script>
+
+<style lang="scss">
+html,
+body {
+  height: 100%;
+  overflow-x: hidden;
+}
+
+body {
+  margin: 0;
+  font-family: "Montserrat", sans-serif;
+}
+
+.container {
+  padding: 1% 2% !important;
+}
+
+.bg {
+  background-image: url("./assets/images/background.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  padding-left: 5%;
+
+}
+</style>
