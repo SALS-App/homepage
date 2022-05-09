7faaff70ab7a2fea24a1b9227731410fd10c70e9
Timm Ortloff <me@tortloff.de>
2022-05-09 06:24:05 +0200

    Fixed vuetify import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import * as vuetify from '@vuetify/vite-plugin'
+import vuetify from '@vuetify/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
