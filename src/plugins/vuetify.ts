74c04ffc227f9d5c1e6a9b7a375944b6f234d8cb
Timm Ortloff <me@tortloff.de>
2022-05-16 06:03:01 +0200

    Refactored theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,19 +5,23 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
+const dark = {
+  dark: true,
+  colors: {
+    background: '#040402',
+    surface: '#000000',
+    primary: '#4285f4',
+    secondary: '#03DAC6',
+    'on-surface': "#000000"
+  }
+}
+
 export default createVuetify(
   {
     theme: {
       defaultTheme: 'dark',
       themes: {
-        dark: {
-          dark: true,
-          colors: {
-            primary: "#4285f4",
-            background: "#040402",
-            'on-surface': "#000000"
-          }
-        },
+        dark
       },
     },
   }
