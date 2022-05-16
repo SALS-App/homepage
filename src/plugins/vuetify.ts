1d47b9194f2d37760553c5126e9268927264a116
Timm Ortloff <me@tortloff.de>
2022-05-16 04:44:42 +0200

    Added on-surface style

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -8,12 +8,14 @@ import { createVuetify } from 'vuetify'
 export default createVuetify(
   {
     theme: {
+      defaultTheme: 'dark',
       themes: {
-        light: {
+        dark: {
           dark: true,
           colors: {
             primary: "#4285f4",
-            background: "#040402"
+            background: "#040402",
+            'on-surface': "#000000"
           }
         },
       },
