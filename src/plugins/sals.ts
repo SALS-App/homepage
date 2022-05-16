e010723e8107fd42618bcaecf9283a5e4da9c793
Timm Ortloff <me@tortloff.de>
2022-05-16 04:44:31 +0200

    Added sals.ts

---
+++ src/plugins/sals.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export async function getDLLink(): Promise<string> {
+    const providerInfo = (await axios.get(
+        `https://update.sals-app.com/latest.yml`
+    )).data;
+
+    const regex = /path: ?([-0-9a-zA-Z .,]){1,420}/gm;
+    let m: any;
+
+    while ((m = regex.exec(providerInfo.toString())) !== null) {
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+
+        return ('https://update.sals-app.com/' + m[0].substring(6));
+    }
+
+    return '';
+}
